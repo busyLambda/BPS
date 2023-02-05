fbef10bf39637715f2d0924bd11feac73d28471c
Susa Milán <mailtosnek@protonmail.com>
2023-02-05 20:45:43 +0100

    Remove and install added along with some args

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use clap::Command;
+use clap::{Arg, Command};
 
 pub mod models;
 
@@ -14,5 +14,25 @@ fn main() {
         .author("Susa Milán")
         .about("A simple binary packing system written in Rust.")
         .before_help(LOGO)
+        .subcommand(
+            Command::new("install").arg(
+                // Force Reinstall
+                Arg::new("force-reinstall")
+                    .long("force-reinstall")
+                    .short('r')
+                    .required(false)
+                    .help("Force BPS to reinstall already present packages on the system."),
+            ),
+        )
+        .subcommand(
+            Command::new("remove")
+            .arg(Arg::new("packages")
+                .required(true)
+            )
+            .arg(Arg::new("purge")
+                .long("purge")
+                .help("Remove all files and dependencies associated with the specified packages. IF ANOTHER PROGRAM REQUIRES THE DEPENDENCIES, THEN IT WILL NOT REMOVE THEM!")
+            )
+    )
         .get_matches();
 }
