b96e056f3e3d3610fbddf0814f01a15db8bf47c1
Susa Milán <mailtosnek@protonmail.com>
2023-02-05 16:35:54 +0100

    Create Database handle

---
+++ src/models/database.rs
@@ -0,0 +1,15 @@
+pub struct Database {
+    pub dbroot: String,
+}
+
+impl Database {
+    pub fn new(dbroot: String) -> Database {
+        Self {
+            dbroot: std::env::var_os("BPS_DBROOT")
+                .expect("Cannot get dbroot from env variable: BPS_DBROOT")
+                .to_str()
+                .expect("BPS_DBROOT cannot be parsed as a str")
+                .to_string()
+        }
+    }
+}
