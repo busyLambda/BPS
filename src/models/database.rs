8ce1322a212687028502f966da2f4e80c9836a17
Susa Milán <mailtosnek@protonmail.com>
2023-02-07 08:43:10 +0100

    read_pkg

--- src/models/database.rs
+++ src/models/database.rs
@@ -1,3 +1,6 @@
+use super::package::Package;
+use std::{fs, io::Read};
+
 pub struct Database {
     pub dbroot: String,
 }
@@ -11,5 +14,25 @@ impl Database {
                 .expect("BPS_DBROOT cannot be parsed as a str")
                 .to_string(),
         }
+    }
+
+    // TODO: Improve error handling.
+    // FIXME: Return a result and an option maybe?
+    pub fn read_pkg(&self, pkg_name: &str) -> Option<Package> {
+        let mut file = match fs::File::open(format!("{}/{}.db", self.dbroot, pkg_name.to_string())) {
+            Ok(f) => f,
+            _ => return None,
+        };
+        let mut buf = String::new();
+
+        match file.read_to_string(&mut buf) {
+            Ok(_) => (),
+            _ => return None,
+        };
+
+        match serde_json::from_str(&buf) {
+            Ok(p) => return Some(p),
+            _ => return None,
+        };
     }
 }
