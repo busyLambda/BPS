e615370cc469f75dc85a37f79af51a980c4c22dd
Susa Milán <mailtosnek@protonmail.com>
2023-02-08 18:51:37 +0100

    mirror handle added

--- src/models.rs
+++ src/models.rs
@@ -2,3 +2,4 @@ pub mod database;
 pub mod dependency;
 pub mod package;
 pub mod version;
+pub mod mirror_handle;
---
+++ src/models/mirror_handle.rs
@@ -0,0 +1,14 @@
+
+// Handles mirrors
+pub struct MirrorHandle {
+    mirrors_dir: String,
+}
+
+impl MirrorHandle {
+    pub fn read_mirrors() {
+
+    }
+    pub fn find_pkg() {
+
+    }
+}
