e9d307b3c40908e6583ee18256cd15e8e86a987b
Susa Milán <mailtosnek@protonmail.com>
2023-02-09 12:19:23 +0100

    Mirrors and MirrorHandle

--- src/models/mirror_handle.rs
+++ src/models/mirror_handle.rs
@@ -1,3 +1,5 @@
+use std::fs::File;
+use serde::{Serialize, Deserialize};
 
 // Handles mirrors
 pub struct MirrorHandle {
@@ -5,10 +7,25 @@ pub struct MirrorHandle {
 }
 
 impl MirrorHandle {
-    pub fn read_mirrors() {
+    pub fn new() -> Self {
+        Self {
+            mirrors_dir: std::env::var_os("BPS_MIRROR_ROOT")
+                .expect("Cannot get mirror root from env variable: BPS_MIRROR_ROOT by default it should be: \"/var/lib/pkg/sync/\"")
+                .to_str()
+                .expect("BPS_MIRROR_ROOT cannot be parsed as a str")
+                .to_string(),
+        }
+    }
+    pub fn read_mirrors(&self) -> Vec<Mirror> {
+        todo!()
+    }
+    pub fn find_pkg(&self, mirror: Mirror) {
 
     }
-    pub fn find_pkg() {
+}
 
-    }
+#[derive(Deserialize, Serialize)]
+pub struct Mirror {
+    name: String,
+    url: String,
 }
