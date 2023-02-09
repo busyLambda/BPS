e0492202a521f57aecc449110ed5bd5efe8f7911
Susa Milán <mailtosnek@protonmail.com>
2023-02-09 12:19:36 +0100

    Package reorganization.

--- src/models/package.rs
+++ src/models/package.rs
@@ -6,7 +6,19 @@ use super::{dependency::Dependency, version::Version};
 pub struct Package {
     pub name: String,
     pub version: Version,
+}
+
+#[derive(Deserialize, Serialize)]
+pub struct MirrorPackage {
+    pub name: String,
+    pub version: Version,
     pub dependencies: Vec<Dependency>,
     pub description: Option<String>,
     pub package_script: String,
+}
+
+impl MirrorPackage {
+    pub fn install(&self) {
+
+    }
 }
